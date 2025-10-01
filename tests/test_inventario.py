7e03648d57a86ebd8a24d4e0aa692caf45e8c8fb
bservin7 <bservin7@gmail.com>
2025-09-30 23:37:27 -0600

    Proyecto de  pruebas y pipeline

--- tests/test_inventario.py
+++ tests/test_inventario.py
@@ -1,4 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.inventario import calcular_stock
+
 
 def test_calcular_stock():
     assert calcular_stock(10, 3) == 7
